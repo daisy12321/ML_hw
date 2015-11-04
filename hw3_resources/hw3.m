50b1f31f5e2cec971c89782b5a28838b2d8bba4b
daisy12321 <candydaisy12321@gmail.com>
2015-11-04 14:54:52 -0500

    start hw3

---
+++ hw3_resources/hw3.m
@@ -0,0 +1,5 @@
+addpath('srcs', 'hw3_resources')
+%set(gcf,'Visible','off')              % turns current figure "off"
+%set(0,'DefaultFigureVisible','off');  % all subsequent figures "off"
+
+%%% 3.1 Neuro networks %%%%

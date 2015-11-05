6052f5e0b9c7a1e06c7c3d04badf67bf43a12ceb
daisy12321 <candydaisy12321@gmail.com>
2015-11-05 16:51:44 -0500

    something that doesn't break

--- hw3_resources/hw3.m
+++ hw3_resources/hw3.m
@@ -3,13 +3,15 @@ addpath('srcs', 'hw3_resources')
 %set(0,'DefaultFigureVisible','off');  % all subsequent figures "off"
 
 %%% 3.1 Neuro networks %%%%
-X = [1, 2; 4, 5; 6, 9; 8, 11];
-Y = [1, 0; 0, 1; 0, 1; 1, 0];
+X = [1, 2; 4, 5; 6, 9; 8, 11; 11, 14; 15, 20];
+Y = [1, 0; 0, 1; 0, 1; 1, 0; 1, 0; 1,0];
 W1 = [1, 2; 3, 5; 5,1];
 W2 = [2 1 1; 5 -1 3];
 
 w1_0 = ones(size(W1));
 w2_0 = ones(size(W2));
+
+grad_desc_3(@ANN_loss, w1_0, w2_0, X, Y, 1, 0.001)
 
 
 importdata('hw3_resources/data/toy_multiclass_1_train.csv')
